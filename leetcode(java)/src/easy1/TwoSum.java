0cc7721199e9f1cb0fb1738c9e0b8d991cdb691b
Yang Liu <19414123@qq.com>
2017-02-28 20:20:02 +0800

    update

--- leetcode(java)/src/easy1/TwoSum.java
+++ leetcode(java)/src/easy1/TwoSum.java
@@ -1,5 +1,8 @@
 package easy1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * AC
  */
@@ -26,11 +29,27 @@ public class TwoSum
         return null;
     }
 
+    public int[] twoSum1(int[] nums, int target)
+    {
+        int[] result = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+
     public static void main(String[] args)
     {
         int[] a = {3, 2, 4};
         int target = 6;
-        int[] b = new TwoSum().twoSum(a, target);
+        int[] b = new TwoSum().twoSum1(a, target);
         for (int c : b)
         {
             System.out.println(c + " ");
